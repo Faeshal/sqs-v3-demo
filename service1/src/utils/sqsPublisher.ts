a1954d66bb1b19611027d0645d58f378104316a9
faeshal <faeshal111@gmail.com>
2024-03-17 11:06:58 +0700

    rename .env

--- service1/src/utils/sqsPublisher.ts
+++ service1/src/utils/sqsPublisher.ts
@@ -8,8 +8,8 @@ log.level = "info";
 const client = new SQSClient({
   region: "ap-southeast-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
 
